e23d3ea7b39266a96938860892d1b2b633330aa2
DharunkumarDhakshinamoorthy <123617653+DharunkumarDhakshinamoorthy@users.noreply.github.com>
2023-01-28 12:12:02 +0530

    Add files via upload
    assignment2

---
+++ assignment2/Mobile.java
@@ -0,0 +1,21 @@
+package assignment2;
+
+public class Mobile {
+	 public static void main(String[] args) {
+		 String mobileBrandName="Redmi 9prime";
+		 String mobileLogo="Mi";
+		 int noOfMobilePiece=1;
+		 short modelNumber=752;
+		 long mobileImeiNumber=9488083682l;
+		 float mobilePrice=12000f;
+		 boolean isDamaged= false;
+		 System.out.println("1.Mobile Name:"+mobileBrandName);
+		 System.out.println("2.Mobile Logo:"+mobileLogo);
+		 System.out.println("3.No.Of Piece:"+noOfMobilePiece);
+		 System.out.println("4.Model Number:"+modelNumber);
+		 System.out.println("5.Mobile Number:"+mobileImeiNumber);
+		 System.out.println("6.Mobile Price:"+mobilePrice);
+		 System.out.println("7.Is there any Damage?:"+isDamaged);
+	}
+
+}
---
+++ assignment2/TwoWheeler.java
@@ -0,0 +1,18 @@
+package assignment2;
+
+public class TwoWheeler {
+	public static void main(String[] args) {
+	int noOfWheels= 2;
+	short noOfmirrors=02;
+	long chassisNumber=56842856l;
+	boolean isPuncture= false;
+	String bikeName="Yamaha RX-100";
+	double runningKm=5462.32;
+	 System.out.println("1.No.Of Wheeles:"+noOfWheels);
+	 System.out.println("2.No.Of Mirrors:"+noOfmirrors);
+	 System.out.println("3.Chassis Number:"+chassisNumber);
+	 System.out.println("4.Is the Wheels are Puncture?:"+isPuncture);
+	 System.out.println("5.Name:"+bikeName);
+	 System.out.println("6.Running KM:"+runningKm);
+	}
+}
