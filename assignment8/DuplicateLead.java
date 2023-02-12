cef6c94fe3f3e99c646ebd14b0f5fe19dfa17999
DharunkumarDhakshinamoorthy <123617653+DharunkumarDhakshinamoorthy@users.noreply.github.com>
2023-02-12 13:31:51 +0530

    Add files via upload
    a8

---
+++ assignment8/DuplicateLead.java
@@ -0,0 +1,43 @@
+package assignment8;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class DuplicateLead {
+	public static void main(String[] args) {
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//input[@id='username']")).sendKeys("demosalesmanager");
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("crmsfa");
+		driver.findElement(By.xpath("//input[@class='decorativeSubmit']")).click();	
+		driver.findElement(By.xpath("//a[contains(text(),'CRM/SFA')]")).click();
+		driver.findElement(By.xpath("//a[text()='Leads']")).click();
+		driver.findElement(By.xpath("//a[text()='Create Lead']")).click();
+		driver.findElement(By.xpath("(//input[@name='companyName'])[2]")).sendKeys("TESTLEAF");
+		driver.findElement(By.xpath("//input[@id='createLeadForm_firstName']")).sendKeys("DHARUNKUMAR");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Dhakshinamoorthy");
+		driver.findElement(By.id("createLeadForm_firstNameLocal")).sendKeys("DHARUN");
+		driver.findElement(By.id("createLeadForm_departmentName")).sendKeys("Testing Domain");
+		driver.findElement(By.id("createLeadForm_description")).sendKeys("I'm Dharun from Tiruchengode");
+		driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("dharunkumard422@gmail.com");
+		WebElement state=driver.findElement(By.id("createLeadForm_generalStateProvinceGeoId"));
+		Select selectState=new Select(state);
+		selectState.selectByVisibleText("New York");
+		driver.findElement(By.name("submitButton")).click();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
+		System.out.println("Title is:"+ driver.getTitle());
+		driver.findElement(By.xpath("(//a[@class='subMenuButton'])[1]")).click();
+		driver.findElement(By.xpath("(//input[@name='companyName'])[2]")).clear();
+		driver.findElement(By.xpath("(//input[@name='companyName'])[2]")).sendKeys("QEAGLE");
+		driver.findElement(By.xpath("//input[@id='createLeadForm_firstName']")).clear();
+		driver.findElement(By.xpath("//input[@id='createLeadForm_firstName']")).sendKeys("ARUN");
+		driver.findElement(By.xpath("//input[@class='smallSubmit']")).click();
+		}
+
+
+}
